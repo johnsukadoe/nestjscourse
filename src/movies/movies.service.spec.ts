5599650fdb18409410472886bef1a8bca6162bba
Miras <bking4234@gmail.com>
2024-08-12 00:32:48 +0500

    test e2e

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -44,6 +44,67 @@ describe('MoviesService', () => {
         expect(e.message).toEqual(`Film with 37923424 not found`)
       }
     })
+  })
+  
+  describe('remove', ()=>{
+    it('should remove film', ()=>{
+      service.create({
+        title:'Something',
+        year:2000,
+        genres:['something']
+      });
+      const allMovies = service.getAll().length
+      service.remove(1);
+      const afterRemove = service.getAll().length
+      expect(afterRemove).toBeLessThan(allMovies)
+    })
     
+    it('should return 404', ()=>{
+      try{
+        const movie = service.remove(37923424)
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException)
+        expect(e.message).toEqual(`Film with 37923424 not found`)
+      }
+    })
+  })
+  
+  
+  describe('create',()=>{
+    it('should create film', ()=>{
+      const allMovie = service.getAll().length
+      service.create({
+        title:'Something',
+        year:2000,
+        genres:['something']
+      });
+      
+      const afterMovie = service.getAll().length;
+      
+      expect(afterMovie).toBeGreaterThan(allMovie)
+    })
+  })
+  
+  describe('update', ()=>{
+    it('should update film', ()=>{
+      service.create({
+        title:'Something',
+        year:2000,
+        genres:['something']
+      });
+      
+      service.update(1, {title:'Miras'})
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Miras')
+    })
+    
+    it('should return 404', ()=>{
+      try{
+        const movie = service.update(37923424, {})
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException)
+        expect(e.message).toEqual(`Film with 37923424 not found`)
+      }
+    })
   })
 });
